53b63ff80f285cd924e80cae32261ef691957ac3
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-06-07 02:26:42 -0700

    feat: add contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,131 @@
+import Layout from "@/components/layout"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
+import Image from "next/image"
+
+export default function ContactPage() {
+  return (
+    <Layout>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl md:text-6xl font-bold text-center mb-12">Contact Me</h1>
+        <Separator className="mb-12" />
+
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-center">
+                Email
+              </CardTitle>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-center w-full">
+                  <Image src="https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/GMailBanner.gif" alt="GMail Banner" width={1500} height={300} className="rounded-md" />
+                </div>
+                <p className="text-muted-foreground">
+                  Email me at: <a href="mailto:mitchellwintrow@gmail.com" className="text-orange-600 hover:text-purple-800 dark:text-orange-400 dark:hover:text-purple-300 font-semibold hover:underline justify-end items-end flex mt-2">mitchellwintrow@gmail.com →</a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-center">
+                GitHub
+              </CardTitle>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-center w-full">
+                  <Image src="https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/GithubBanner.gif" alt="GitHub Banner" width={1500} height={300} className="rounded-md" />
+                </div>
+                <p className="text-muted-foreground">
+                  Find me on GitHub at: <a href="https://github.com/mrrobotisreal" className="text-orange-600 hover:text-purple-800 dark:text-orange-400 dark:hover:text-purple-300 font-semibold hover:underline justify-end items-end flex mt-2">mrrobotisreal →</a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-center">
+                LinkedIn
+              </CardTitle>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-center w-full">
+                  <Image src="https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/LinkedInBanner.gif" alt="LinkedIn Banner" width={1500} height={300} className="rounded-md" />
+                </div>
+                <p className="text-muted-foreground">
+                  Find me on LinkedIn at: <a href="https://www.linkedin.com/in/mitchell-wintrow/" className="text-orange-600 hover:text-purple-800 dark:text-orange-400 dark:hover:text-purple-300 font-semibold hover:underline justify-end items-end flex mt-2">Mitchell Wintrow →</a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-center">
+                X (Twitter)
+              </CardTitle>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-center w-full">
+                  <Image src="https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/XBanner.gif" alt="X (Twitter) Banner" width={1500} height={300} className="rounded-md" />
+                </div>
+                <p className="text-muted-foreground">
+                  Find me on X (Twitter) at: <a href="https://x.com/mitchwintrow" className="text-orange-600 hover:text-purple-800 dark:text-orange-400 dark:hover:text-purple-300 font-semibold hover:underline justify-end items-end flex mt-2">@mitchwintrow →</a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-center">
+                Medium
+              </CardTitle>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-center w-full">
+                  <Image src="https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/MediumBanner.gif" alt="Medium Banner" width={1500} height={300} className="rounded-md" />
+                </div>
+                <p className="text-muted-foreground">
+                  Read my articles on Medium at: <a href="https://medium.com/@90mitchwintrow" className="text-orange-600 hover:text-purple-800 dark:text-orange-400 dark:hover:text-purple-300 font-semibold hover:underline justify-end items-end flex mt-2">@90mitchwintrow →</a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <CardTitle className="flex justify-center">
+                YouTube
+              </CardTitle>
+              <Separator />
+            </CardHeader>
+            <CardContent>
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-center w-full">
+                  <Image src="https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/YouTubeBanner.gif" alt="YouTube Banner" width={1500} height={300} className="rounded-md" />
+                </div>
+                <p className="text-muted-foreground">
+                  Watch my videos on YouTube at: <a href="https://www.youtube.com/@mitchwintrow" className="text-orange-600 hover:text-purple-800 dark:text-orange-400 dark:hover:text-purple-300 font-semibold hover:underline justify-end items-end flex mt-2">Mitch Wintrow →</a>
+                </p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </Layout>
+  )
+}
--- components/blocks/Components/ProfileCard/ProfileCard.tsx
+++ components/blocks/Components/ProfileCard/ProfileCard.tsx
@@ -30,8 +30,10 @@ interface ProfileCardProps {
 const DEFAULT_BEHIND_GRADIENT =
   "radial-gradient(farthest-side circle at var(--pointer-x) var(--pointer-y),hsla(266,100%,90%,var(--card-opacity)) 4%,hsla(266,50%,80%,calc(var(--card-opacity)*0.75)) 10%,hsla(266,25%,70%,calc(var(--card-opacity)*0.5)) 50%,hsla(266,0%,60%,0) 100%),radial-gradient(35% 52% at 55% 20%,#00ffaac4 0%,#073aff00 100%),radial-gradient(100% 100% at 50% 50%,#00c1ffff 1%,#073aff00 76%),conic-gradient(from 124deg at 50% 50%,#c137ffff 0%,#07c6ffff 40%,#07c6ffff 60%,#c137ffff 100%)";
 
+// const DEFAULT_INNER_GRADIENT =
+//   "linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)";
 const DEFAULT_INNER_GRADIENT =
-  "linear-gradient(145deg,#60496e8c 0%,#71C4FF44 100%)";
+  "linear-gradient(145deg,oklch(64.6% .222 41.116) 0%,oklch(38.1% .176 304.987) 100%)";
 
 const ANIMATION_CONFIG = {
   SMOOTH_DURATION: 600,
