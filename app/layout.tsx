0c1fd6f9b60f34f58010f37a224f0b2f087ba683
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-06-09 20:17:50 -0700

    feat: add google verification code

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,32 +15,53 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "WinApps.io - Mitchell Wintrow",
-  description: "Welcome to WinApps.io! I'm Mitchell Wintrow, a software engineer specializing in mobile apps, web development, and cloud solutions. Explore my portfolio, projects, and technical blog posts.",
-  keywords: ["Mitchell Wintrow", "WinApps", "Software Engineer", "Mobile Apps", "Web Development", "React", "React Native", "AWS", "Portfolio", "Next.js", "Blog", "Go", "Golang", "MySQL", "PostgreSQL", "MongoDB", "Node.js"],
+  description:
+    "Welcome to WinApps.io! I'm Mitchell Wintrow, a software engineer specializing in mobile apps, web development, and cloud solutions. Explore my portfolio, projects, and technical blog posts.",
+  keywords: [
+    "Mitchell Wintrow",
+    "WinApps",
+    "Software Engineer",
+    "Mobile Apps",
+    "Web Development",
+    "React",
+    "React Native",
+    "AWS",
+    "Portfolio",
+    "Next.js",
+    "Blog",
+    "Go",
+    "Golang",
+    "MySQL",
+    "PostgreSQL",
+    "MongoDB",
+    "Node.js",
+  ],
   authors: [{ name: "Mitchell Wintrow" }],
   creator: "Mitchell Wintrow",
   publisher: "WinApps.io",
   openGraph: {
     title: "WinApps.io - Mitchell Wintrow",
-    description: "Software engineer specializing in mobile apps, web development, and cloud solutions",
+    description:
+      "Software engineer specializing in mobile apps, web development, and cloud solutions",
     type: "website",
     locale: "en_US",
     url: "https://www.wintrow.dev",
     siteName: "WinApps.io",
     images: [
       {
-        url: 'https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/WinAppsOpenGraph.jpg',
+        url: "https://winapps-solutions-llc.s3.us-west-2.amazonaws.com/images/WinAppsOpenGraph.jpg",
         width: 1200,
         height: 630,
-        alt: 'Mitchell Wintrow',
+        alt: "Mitchell Wintrow",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
     title: "WinApps.io - Mitchell Wintrow",
-    description: "Software engineer specializing in mobile apps, web development, and cloud solutions",
-    images: ['https://www.wintrow.dev/WinAppsOpenGraph.jpg'],
+    description:
+      "Software engineer specializing in mobile apps, web development, and cloud solutions",
+    images: ["https://www.wintrow.dev/WinAppsOpenGraph.jpg"],
   },
   robots: {
     index: true,
@@ -48,13 +69,14 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   verification: {
-    google: 'your-google-verification-code',
+    google:
+      "google-site-verification=NYEhSNKfUT2whyHI30oxOlNXrQ4_jbzTKiwmrQAP_7s",
   },
 };
 
@@ -75,9 +97,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
