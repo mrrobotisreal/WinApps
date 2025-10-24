ff42eb93ac0d49a3f84fe936453f7a2ad6b23c73
Mitchell Wintrow <90mitchwintrow@gmail.com>
2025-10-23 21:51:28 -0600

    Update to use NewestIntroVideo_720.mp4 on home page

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
             !
           </h1>
 
-          {/* Intro Video */}
+          {/* Newest Intro Video */}
           <div className="max-w-4xl mx-auto rounded-lg overflow-hidden shadow-2xl">
             <video
               src="https://pub-5e3f5f69f6bd4f2fb6bc741e03f34851.r2.dev/NewestIntroVideo_720.mp4"
