9aebfa5d3a4765d1b545f807bf5fa96dc1d0f804
Mitchell Wintrow <90mitchwintrow@gmail.com>
2026-01-20 13:06:19 -0700

    Update footer copyright from 2025 to 2026

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="bg-black/95 backdrop-blur supports-[backdrop-filter]:bg-black/60">
       <div className="container mx-auto px-4 py-2 flex flex-col items-center justify-center">
         <p className="text-orange-400 dark:text-white">
-          © 2025{" "}
+          © 2026{" "}
           <span className="bg-gradient-to-b from-orange-300 to-orange-900 bg-clip-text text-transparent font-bold">
             Win
           </span>
@@ -12,7 +12,9 @@ export default function Footer() {
           </span>
         </p>
         <p className="text-orange-400 dark:text-white">All Rights Reserved</p>
-        <p className="text-orange-400 dark:text-white">mitchellwintrow@gmail.com</p>
+        <p className="text-orange-400 dark:text-white">
+          mitchellwintrow@gmail.com
+        </p>
       </div>
     </footer>
   );
